1a1b593c3d65f5152c4b59f7b9311a37261de994
funyoo <yorkfang@foxmail.com>
2019-12-18 20:38:04 +0800

    缓存key

---
+++ src/main/java/com/funyoo/hqxApp/util/cachePool/CacheKeys.java
@@ -0,0 +1,21 @@
+package com.funyoo.hqxApp.util.cachePool;
+
+import java.lang.reflect.Method;
+
+public class CacheKeys {
+
+    public static String ARTICLE_NEWS_TOP = "ARTICLE_NEWS_TOP";
+    public static String ARTICLE_COMMEND_TOP = "ARTICLE_COMMEND_TOP";
+    public static String ARTICLE_MILITARY_TOP = "ARTICLE_MILITARY_TOP";
+    public static String ARTICLE_HISTORY_TOP = "ARTICLE_HISTORY_TOP";
+    public static String ARTICLE_CHARACTER_TOP = "ARTICLE_CHARACTER_TOP";
+
+    public static String ARTICLE_NEWS_LIST = "ARTICLE_NEWS_LIST";
+    public static String ARTICLE_COMMEND_LIST = "ARTICLE_COMMEND_LIST";
+    public static String ARTICLE_MILITARY_LIST = "ARTICLE_MILITARY_LIST";
+    public static String ARTICLE_HISTORY_LIST = "ARTICLE_HISTORY_LIST";
+    public static String ARTICLE_CHARACTER_LIST = "ARTICLE_CHARACTER_LIST";
+
+    public static String SCHOOL_ACTIVITY_LIST = "SCHOOL_ACTIVITY_LIST";
+
+}
