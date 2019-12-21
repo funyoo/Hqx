004d6758ef8ff60e6a6a9b8f68e9e203d63bffb5
funyoo <yorkfang@foxmail.com>
2019-12-21 13:21:04 +0800

    增加文章查询缓存与数据库查询日志

--- src/main/java/com/funyoo/hqxApp/controller/AppNewsViewController.java
+++ src/main/java/com/funyoo/hqxApp/controller/AppNewsViewController.java
@@ -1,9 +1,16 @@
 package com.funyoo.hqxApp.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.funyoo.hqxApp.aspect.LogAspect;
 import com.funyoo.hqxApp.model.Article;
 import com.funyoo.hqxApp.result.CodeMsg;
 import com.funyoo.hqxApp.result.Result;
 import com.funyoo.hqxApp.service.ArticleReadSevice;
+import com.funyoo.hqxApp.util.cachePool.CacheKeys;
+import com.funyoo.hqxApp.util.cachePool.CachePool;
+import com.funyoo.hqxApp.util.cachePool.CachePoolInterface;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +25,11 @@ public class AppNewsViewController {
     @Autowired
     ArticleReadSevice viewSevices;
 
+    @Autowired
+    CachePoolInterface cachePool;
+
+    private Logger logger = LogManager.getLogger(AppNewsViewController.class);
+
     /**
      * 查询相关模块文章列表
      * @param part 模块名称
@@ -29,10 +41,19 @@ public class AppNewsViewController {
     public Result<List<Article>> getArticles(String part, Integer index) {
         //查询相关模块文章列表，返回list
         List<Article> articles;
+        String listStr;
+        String key = CacheKeys.ARTICLE_LIST + part + index;
+        if ((listStr = (String) cachePool.get(key)) != null) {
+            // 内存缓存有数据
+            return Result.success(JSON.parseObject(listStr, List.class));
+        }
+
+        logger.info("[DO_SQL]" + "[getArticles] [" + part + ", " + index + "]");
         articles = viewSevices.getArticle(part, index);
         if (articles == null || articles.size() == 0) {
             return Result.error(CodeMsg.NO_ARTICLE_DATA);
         }
+        cachePool.put(key, JSON.toJSONString(articles));
         return Result.success(articles);
     }
 
@@ -46,10 +67,18 @@ public class AppNewsViewController {
     public Result<Article> getTopArticle(String part) {
         //查询首页头条文章，带图片，返回文章实例
         Article article;
+        String topStr;
+        String key = CacheKeys.ARTICLE_TOP + part;
+        if ((topStr = (String) cachePool.get(key)) != null) {
+            return Result.success(JSON.parseObject(topStr, Article.class));
+        }
+
+        logger.info("[DO_SQL]" + "[getTopArticle] [" + part + "]");
         article = viewSevices.getTopArticle(part);
         if (article == null || article.getPicUrl().isEmpty()) {
             return Result.error(CodeMsg.GET_TOP_VIEW_ERR);
         }
+        cachePool.put(key, JSON.toJSONString(article));
         return Result.success(article);
     }
 }
