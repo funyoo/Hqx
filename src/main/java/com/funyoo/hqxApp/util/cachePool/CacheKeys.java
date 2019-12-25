d45f148b477388e0035313bcedb5c07f11073d31
funyoo <yorkfang@foxmail.com>
2019-12-25 16:18:44 +0800

    增加了最新文章的缓存key

--- src/main/java/com/funyoo/hqxApp/util/cachePool/CacheKeys.java
+++ src/main/java/com/funyoo/hqxApp/util/cachePool/CacheKeys.java
@@ -8,6 +8,8 @@ public class CacheKeys {
 
     public static String ARTICLE_LIST = "ARTICLE_LIST";
 
+    public static String NEW_ARTICLE_LIST = "NEW_ARTICLE_LIST";
+
 
     public static String SCHOOL_ACTIVITY_LIST = "SCHOOL_ACTIVITY_LIST";
 
