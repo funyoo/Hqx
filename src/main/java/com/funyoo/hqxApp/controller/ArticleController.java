c6492e10b754f6d155dade497cea32a40ed9b582
funyoo <yorkfang@foxmail.com>
2019-12-25 16:17:05 +0800

    增加模糊查询文章标题的接口

--- src/main/java/com/funyoo/hqxApp/controller/ArticleController.java
+++ src/main/java/com/funyoo/hqxApp/controller/ArticleController.java
@@ -1,10 +1,16 @@
 package com.funyoo.hqxApp.controller;
 
+import com.funyoo.hqxApp.model.Article;
+import com.funyoo.hqxApp.result.Result;
 import com.funyoo.hqxApp.service.ArticleReadSevice;
 import com.funyoo.hqxApp.vo.ArticleHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/article")
@@ -17,5 +23,18 @@ public class ArticleController {
     public void counter(String part, Integer id) {
         ArticleHelper articleHelper = new ArticleHelper(part, id);
         readSevice.counter(articleHelper);
+    }
+
+    /**
+     * 查找文章
+     * @param str
+     * @return
+     */
+    @RequestMapping("/search")
+    @ResponseBody
+    public Result<List<Article>> getNewArticles(String str) {
+        List<Article> articles = new ArrayList<>();
+        articles = readSevice.search(str);
+        return Result.success(articles);
     }
 }
